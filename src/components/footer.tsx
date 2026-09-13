7b78b56178cd99121bc0d821a9d6db23876544a5
avansear <work@avansear.com>
2026-09-13 17:34:44 -0400

    url

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ export function Footer() {
     <footer className="page py-16">
       <p>
         new website, under construction. to visit old website, go to{" "}
-        <a href="https://www-avansear.vercel.app/" className="underline">
-          https://www-avansear.vercel.app/
+        <a href="https://old.avansear.com/" className="underline">
+          old.avansear.com
         </a>
       </p>
     </footer>
